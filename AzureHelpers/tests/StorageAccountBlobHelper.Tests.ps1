c92b20538837ba2d61b7f773d25360d947203652
Ryan Haworth <rhaworth211@msn.com>
2025-04-27 19:03:17 -0400

    split describe

--- AzureHelpers/tests/StorageAccountBlobHelper.Tests.ps1
+++ AzureHelpers/tests/StorageAccountBlobHelper.Tests.ps1
@@ -1,4 +1,4 @@
-BeforeAll {    
+BeforeAll {
     $ModulePath = Join-Path $PSScriptRoot "..\StorageAccountBlobHelper.psm1"
 
     if (-not (Test-Path $ModulePath)) {
@@ -21,80 +21,69 @@ Describe "StorageAccountBlobHelper Module Tests" {
             (Get-ClientId) | Should -BeNullOrEmpty
         }
     }
+}
 
-    Context "Get-AccessToken Tests" {
-        Context "When token is expired" {
-            BeforeAll {                
-                Mock -CommandName Invoke-WithRetry -ModuleName StorageAccountBlobHelper { @{ access_token = "mock-token"; expires_in = 3600 } }
-            }
-    
-            It "Should retrieve and cache an access token" {
-                $ExecutionContext.SessionState.PSVariable.Set('script:AccessToken', $null)
-                $ExecutionContext.SessionState.PSVariable.Set('script:AccessTokenExpiry', (Get-Date).AddMinutes(-5))
-    
-                $token = Get-AccessToken
-                $token | Should -Be "mock-token"
-            }
-        }
-    
-        Context "When token is valid and cached" {
-            BeforeAll {                
-                Mock -CommandName Invoke-WithRetry -ModuleName StorageAccountBlobHelper { throw "Invoke-WithRetry should not be called if token is cached!" }
-            }
-    
-            It "Should use cached token if not expired" {
-                $ExecutionContext.SessionState.PSVariable.Set('script:AccessToken', 'cached-token')
-                $ExecutionContext.SessionState.PSVariable.Set('script:AccessTokenExpiry', (Get-Date).AddMinutes(30))
-    
-                $token = Get-AccessToken
-                $token | Should -Be "cached-token"
-            }
-        }
-    }
-            
-
-    Context "New-Blob Tests" {
-        BeforeEach {
-            Mock Get-AccessToken { "mock-token" }
-            Mock Invoke-WithRetry { }
-        }
-
-        It "Should call Invoke-WithRetry with correct parameters when creating blob" {
-            $filePath = "$env:TEMP\mockfile.txt"
-            Set-Content -Path $filePath -Value "Test content"
-
-            New-Blob -StorageAccountName "mockstorage" -ContainerName "mockcontainer" -BlobName "mockblob.txt" -FilePath $filePath
-
-            Assert-MockCalled Invoke-WithRetry -Times 1 -Exactly
-            Remove-Item $filePath
-        }
+Describe "Get-AccessToken Tests - When token is expired" {
+    BeforeAll {        
+        Mock -CommandName Invoke-WithRetry -ModuleName StorageAccountBlobHelper { @{ access_token = "mock-token"; expires_in = 3600 } }
     }
 
-    Context "Get-Blob Tests" {
-        BeforeEach {
-            Mock Get-AccessToken { "mock-token" }
-            Mock Invoke-WithRetry { }
-        }
+    It "Should retrieve and cache an access token when expired" {
+        $ExecutionContext.SessionState.PSVariable.Set('script:AccessToken', $null)
+        $ExecutionContext.SessionState.PSVariable.Set('script:AccessTokenExpiry', (Get-Date).AddMinutes(-5))
 
-        It "Should call Invoke-WithRetry when downloading blob" {
-            $downloadPath = "$env:TEMP\downloadedfile.txt"
-
-            Get-Blob -StorageAccountName "mockstorage" -ContainerName "mockcontainer" -BlobName "mockblob.txt" -DownloadPath $downloadPath
-
-            Assert-MockCalled Invoke-WithRetry -Times 1 -Exactly
-        }
-    }
-
-    Context "Remove-Blob Tests" {
-        BeforeEach {
-            Mock Get-AccessToken { "mock-token" }
-            Mock Invoke-WithRetry { }
-        }
-
-        It "Should call Invoke-WithRetry to delete blob" {
-            Remove-Blob -StorageAccountName "mockstorage" -ContainerName "mockcontainer" -BlobName "mockblob.txt"
-
-            Assert-MockCalled Invoke-WithRetry -Times 1 -Exactly
-        }
+        $token = Get-AccessToken
+        $token | Should -Be "mock-token"
     }
 }
+
+Describe "Get-AccessToken Tests - When token is valid and cached" {
+    It "Should use cached token if not expired" {
+        $ExecutionContext.SessionState.PSVariable.Set('script:AccessToken', 'cached-token')
+        $ExecutionContext.SessionState.PSVariable.Set('script:AccessTokenExpiry', (Get-Date).AddMinutes(30))
+
+        $token = Get-AccessToken
+        $token | Should -Be "cached-token"
+    }
+}
+
+Describe "New-Blob Tests" {
+    BeforeAll {
+        Mock -CommandName Get-AccessToken -ModuleName StorageAccountBlobHelper { "mock-token" }
+        Mock -CommandName Invoke-WithRetry -ModuleName StorageAccountBlobHelper { }
+    }
+
+    It "Should call Invoke-WithRetry with correct parameters when creating blob" {
+        $filePath = "$env:TEMP\mockfile.txt"
+        Set-Content -Path $filePath -Value "Test content"
+
+        New-Blob -StorageAccountName "mockstorage" -ContainerName "mockcontainer" -BlobName "mockblob.txt" -FilePath $filePath
+
+        Assert-MockCalled Invoke-WithRetry -Times 1 -Exactly
+        Remove-Item $filePath
+    }
+}
+
+Describe "Get-Blob Tests" {
+    BeforeAll {
+        Mock -CommandName Get-AccessToken -ModuleName StorageAccountBlobHelper { "mock-token" }
+        Mock -CommandName Invoke-WithRetry -ModuleName StorageAccountBlobHelper { }
+    }
+
+    It "Should call Invoke-WithRetry when downloading blob" {
+        $downloadPath = "$env:TEMP\downloadedfile.txt"
+
+        Get-Blob -StorageAccountName "mockstorage" -ContainerName "mockcontainer" -BlobName "mockblob.txt" -DownloadPath $downloadPath
+
+        Assert-MockCalled Invoke-WithRetry -Times 1 -Exactly
+    }
+}
+
+Describe "Remove-Blob Tests" {
+    BeforeAll {
+        Mock -CommandName Get-AccessToken -ModuleName StorageAccountBlobHelper { "mock-token" }
+        Mock -CommandName Invoke-WithRetry -ModuleName StorageAccountBlobHelper { }
+    }
+
+    It "Should call Invoke-WithRetry to delete blob" {
+        Remove-Blob -StorageAccountName "mockstorage" -ContainerName "mockcontainer" -BlobN
